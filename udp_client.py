0dc7a8f20f6ed5645108566f20e67a8663442349
Asasino <4ertugrulkara6@gmail.com>
2017-12-23 17:11:34 +0300

    imp

--- udp_client.py
+++ udp_client.py
@@ -8,27 +8,9 @@ def utf8len(s):
     return len(s.encode('utf-8'))
 
 
-def client(ip, port, message):
-    start = timer()
-    try:
-        # Send message
-        sock.sendto(message, (ip, port))
-
-        # Wait for ACK or NACK message.
-        response = sock.recv(1024)
-    # print "ACKReceivedU1: {}".format(response)
-    except:
-        response = "NACK_U1_to_GATEWAY"
-    finally:
-        sock.close()
-    end = timer()
-    if "NACK" in response:
-        print response
-        # print (end - start) * 1000
-
-
 class RDT_UDPClient:
-    dest_ip = ""
+    dest_ip = ["10.10.4.1", "10.10.3.2"]
+    dest_ip_index = 0
     dest_port = 8765
     file_to_send = "5mb.txt"
     seq_to_send = 0
@@ -44,19 +26,48 @@ class RDT_UDPClient:
         self.ack_came = 0
         self.file = None
         # Open socket
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.settimeout(0.5)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.settimeout(0.5)
 
     def _initial_packet(self):
         self.file = open(self.file_to_send, 'r')
         self._headers["file_name"] = self.file_to_send
         self._headers["size_bytes"] = utf8len(self.file.read())
+        self.seq_to_send += 1
 
     def _middle_packets(self):
-        pass
+        data = self.file.read(600)
+        self._headers["seq"] = self.seq_to_send
+        self._data = data
+        self.seq_to_send += 600
+
+    def _prepare_packet(self):
+        self._headers["seq"] = self.seq_to_send
+        if self.seq_to_send <= 0:
+            self._initial_packet()
+        else:
+            self._middle_packets()
+        self.message = {"headers": self._headers, "payload": self._data}
 
     def send_file(self, file_name="5mb.txt"):
         self.file_to_send = file_name
+        self._prepare_packet()
+        try:
+            # Send message
+            self.sock.sendto(self.message, (self.dest_ip[self.dest_ip_index], self.dest_port))
+            self.response = self.sock.recv(1024)
+            self._check_incoming_ack()
+        except: # Timeout
+            pass
+
+    def _check_incoming_ack(self):
+        incoming_ack = self.response["headers"]["ack"]
+        self.ack_came = incoming_ack
+        if incoming_ack  == self.seq_to_send:
+            pass #Basarili
+        else:
+            # Bigger or lower ack
+            self.seq_to_send = incoming_ack
 
 
 
--- udp_server.py
+++ udp_server.py
@@ -4,120 +4,115 @@ import time
 
 
 def utf8len(s):
-	return len(s.encode('utf-8'))
+    return len(s.encode('utf-8'))
 
 
 class RDT_UDPHandler(SS.BaseRequestHandler):
-	file_name = "default.txt"
-	file_size = 0
-	file = None
-	last_succ_byte = 0
-	waiting_for_byte = last_succ_byte + 1
-	buffer = []
-	package_coming = []
+    file_name = "default.txt"
+    file_size = 0
+    file = None
+    last_succ_byte = 0
+    waiting_for_byte = last_succ_byte + 1
+    buffer = []
+    package_coming = []
 
-	def _init(self):
-		self.file_name = self._headers["file_name"]
-		self.file_size = self._headers["size_bytes"]
-		self.file = open(self.file_name, 'wb')
+    def _init(self):
+        self.file_name = self._headers["file_name"]
+        self.file_size = self._headers["size_bytes"]
+        self.file = open(self.file_name, 'wb')
 
-	def _finish(self):
-		if self.last_succ_byte != self.file_size:
-			return False
-		self.file.close()
-		return True
+    def _finish(self):
+        if self.last_succ_byte != self.file_size:
+            return False
+        self.file.close()
+        return True
 
-	def __received_bytes__(self, bytes):
-		self.last_succ_byte += bytes
-		self.waiting_for_byte = self.last_succ_byte + 1
+    def __received_bytes__(self, bytes):
+        self.last_succ_byte += bytes
+        self.waiting_for_byte = self.last_succ_byte + 1
 
-	def __check_send_ACK__(self):
-		coming_seq_number = self._headers["seq"]
+    def __check_send_ACK__(self):
+        coming_seq_number = self._headers["seq"]
+        msg_bytes = utf8len(self._message)
+        if coming_seq_number == 0:
+            # Initial packet has arrived.
+            # Get properties.
+            self._init()
+            self.__received_bytes__(1)
+        elif coming_seq_number + 1 == self.waiting_for_byte:
+            # Expected package has arrived.
+            # Update ACK message to send.
+            self.__received_bytes__(msg_bytes)
+            self.file.write(self._message)
+            # Write buffered messages to file.
+            self.buffer.sort(key=lambda tup: tup[0])
+            for buffered_item in self.buffer:
+                self.file.write(buffered_item[1])
+                msg_bytes = utf8len(buffered_item[1])
+                self.__received_bytes__(msg_bytes)
+            self.buffer = []
+        elif coming_seq_number + 1 > self.waiting_for_byte:
+            # A packet that is ahead of me has arrived.
+            # But save incoming packet to be processed later.
+            self.buffer.append((coming_seq_number, self._message))
+        elif coming_seq_number + 1 < self.waiting_for_byte:
+            # Already arrived packet came again.
+            # Just send the same ACK.
+            pass
+        elif coming_seq_number + msg_bytes == self.file_size:
+            self._finish()  # Finished.
+            self.waiting_for_byte = -1
+        else:
+            print self._headers
+            print self._data
+            print "ERROR!"
+            raise NotImplementedError
 
-		if coming_seq_number == 0:
-			# Initial packet has arrived.
-			# Get properties.
-			self._init()
-			self.__received_bytes__(1)
-		elif coming_seq_number + 1 == self.waiting_for_byte:
-			# Expected package has arrived.
-			# Update ACK message to send.
-			msg_bytes = utf8len(self._message)
-			self.__received_bytes__(msg_bytes)
-			self.file.write(self._message)
-			# Write buffered messages to file.
-			self.buffer.sort(key=lambda tup: tup[0])
-			for buffered_item in self.buffer:
-				self.file.write(buffered_item[1])
-				msg_bytes = utf8len(buffered_item[1])
-				self.__received_bytes__(msg_bytes)
-			self.buffer = []
-		elif coming_seq_number + 1 > self.waiting_for_byte:
-			# A packet that is ahead of me has arrived.
-			# But save incoming packet to be processed later.
-			self.buffer.append((coming_seq_number, self._message))
-		elif coming_seq_number + 1 < self.waiting_for_byte:
-			# Already arrived packet came again.
-			# Just send the same ACK.
-			pass
-		elif self._headers["last"]:
-			if self._finish():	# Finished.
-				self.waiting_for_byte = -1
-			else:				# Not finished. Send last ACK.
-				pass
-		else:
-			print self._headers
-			print self._data
-			print "ERROR!"
-			raise NotImplementedError
+        # Send new ACK.
+        self._send(self.waiting_for_byte)
 
-		# Send new ACK.
-		self._send(self.waiting_for_byte)
+    def _send(self, seq):
+        socket = self.request[1]
+        response = {"header":
+                        {"ack": str(seq)},
+                    "payload": ""
+                    }
+        socket.sendto(response, self.client_address)
 
-	def _send(self, seq):
-		socket = self.request[1]
-		response = {"header":
-			            {"ack": str(seq),
-			             "last": False},
-		            "payload": ""
-		            }
-		# TODO: Sender also must wait for 'last:True' message from server.
-		socket.sendto(response, self.client_address)
+    def handle(self):
+        # Function to run when new UDP request came to the server.
 
-	def handle(self):
-		# Function to run when new UDP request came to the server.
+        # Extract request
+        self._data = self.request[0].strip()
+        self._headers = self._data["header"]
+        self._message = self._data["payload"]
 
-		# Extract request
-		self._data = self.request[0].strip()
-		self._headers = self._data["header"]
-		self._message = self._data["payload"]
+        # TODO: Checksum?
 
-		# TODO: Checksum?
-
-		self.__check_send_ACK__()
+        self.__check_send_ACK__()
 
 
 class ThreadedUDPServer(SS.ThreadingMixIn, SS.UDPServer):
-	pass
+    pass
 
 
 if __name__ == "__main__":
-	# TODO: Multi-homing? Ana bir UDP portu olup buraya gelen her initial istek icin yeni socket acip,
-	# TODO: sonrasinda gelen paketleri oralara yonlendirme gibi bir sey?
-	USED_PORT = 8765
-	HOST, PORT = "", USED_PORT
+    # TODO: Multi-homing? Ana bir UDP portu olup buraya gelen her initial istek icin yeni socket acip,
+    # TODO: sonrasinda gelen paketleri oralara yonlendirme gibi bir sey?
+    USED_PORT = 8765
+    HOST, PORT = "", USED_PORT
 
-	# Open threaded-server for link.
-	server = ThreadedUDPServer((HOST, PORT), RDT_UDPHandler)
-	ip, port = server.server_address
+    # Open threaded-server for link.
+    server = ThreadedUDPServer((HOST, PORT), RDT_UDPHandler)
+    ip, port = server.server_address
 
-	# Start a thread with the server -- that thread will then start one
-	# more thread for each request
-	server_thread = threading.Thread(target=server.serve_forever)
-	# Exit the server thread when the main thread terminates
-	server_thread.daemon = True
-	server_thread.start()
-	print "UDP Server running."
-	while (1):
-		# Do not close the main thread. Allows us to close all the threads with CTRL+C
-		time.sleep(100)
+    # Start a thread with the server -- that thread will then start one
+    # more thread for each request
+    server_thread = threading.Thread(target=server.serve_forever)
+    # Exit the server thread when the main thread terminates
+    server_thread.daemon = True
+    server_thread.start()
+    print "UDP Server running."
+    while (1):
+        # Do not close the main thread. Allows us to close all the threads with CTRL+C
+        time.sleep(100)
