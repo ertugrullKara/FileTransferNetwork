1f16f789ef02be6e594368eebf94a0720e7a5557
Asasino <4ertugrulkara6@gmail.com>
2017-12-23 23:44:34 +0300

    fix

--- sctp_client.py
+++ sctp_client.py
@@ -1,15 +1,19 @@
 import socket
-
 import sctp
 
 
 class SCTPHandler:
-	def __init__(self, ip, port, filename):
-		self.dest_ip = ip
-		self.dest_port = port
+	def __init__(self, filename):
+		self.dest_ip_port_tuples = [["10.10.2.2", "8765"], ["10.10.4.2", "8765"]]
 		self.filename = filename
+		self.file = open(self.filename, 'wb')
 
 	def send(self):
 		sock = sctp.sctpsocket_tcp(socket.AF_INET)
-		# sctp.bindx ?
-		sock.connectx([(self.dest_ip, self.dest_port)])
+		sock.bindx(self.dest_ip_port_tuples)
+		sock.connectx(self.dest_ip_port_tuples)
+		sock.send(self.file.read())
+
+if __name__ == "__main__":
+	sctp_client = SCTPHandler("5mb.txt")
+	sctp_client.send()
---
+++ sctp_server.py
@@ -0,0 +1,26 @@
+import socket
+import sctp
+
+
+class SCTPHandler:
+    def __init__(self, filename):
+        self.port = "8765"
+        self.filename = filename
+        self.file = open(self.filename, 'wb')
+
+    def serve(self):
+        sock = sctp.sctpsocket_tcp(socket.AF_INET)
+        sock.bind(("", self.port))
+        sock.listen(10)
+
+        while 1:
+            connection, address = sock.accept()
+            buf = connection.recv(1024)
+            if len(buf) > 0:
+                print buf
+                break
+
+
+if __name__ == "__main__":
+    sctp_server = SCTPHandler("5mb.txt")
+    sctp_server.serve()
