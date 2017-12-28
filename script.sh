b0acb2af4427f70c8ac63905817b4733303a983a
e2099125 <e2099125@s.sahw1.ch-geni-net.geni.case.edu>
2017-12-28 05:40:37 -0600

    script

---
+++ script.sh
@@ -0,0 +1,25 @@
+mkdir res/reorder_5_udp
+mkdir res/reorder_5_sctp
+
+for i in {1..2};
+do
+	python udp_client.py input.txt 1 > res/reorder_5_udp/reorder_5_udp_exp1_"${i}".txt
+	echo "udp exp1 $i done"
+done
+
+for i in {1..2};
+do
+	python udp_client.py input.txt 2 > res/reorder_5_udp/reorder_5_udp_exp2_"${i}".txt
+	echo "udp exp2 $i done"
+done
+
+for i in {1..2};
+do
+	python sctp_client.py input.txt 1 > res/reorder_5_sctp/reorder_5_sctp_exp1_"${i}".txt
+	echo "sctp exp1 $i done"
+done
+for i in {1..2};
+do
+	python sctp_client.py input.txt 2 > res/reorder_5_sctp/reorder_5_sctp_exp2_"${i}".txt
+	echo "sctp exp2 $i done"
+done
