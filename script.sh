de0876f70b0f4278765ec7b03b7420db233b7c50
Asasino <4ertugrulkara6@gmail.com>
2017-12-28 14:43:52 +0300

    comments

--- script.sh
+++ script.sh
@@ -1,25 +1,25 @@
-mkdir res/reorder_20_udp
-mkdir res/reorder_20_sctp
+mkdir res/reorder_35_udp
+mkdir res/reorder_35_sctp
 
 for i in {1..2};
 do
-	python udp_client.py input.txt 1 > res/reorder_20_udp/reorder_20_udp_exp1_"${i}".txt
+	python udp_client.py input.txt 1 > res/reorder_35_udp/reorder_35_udp_exp1_"${i}".txt
 	echo "udp exp1 $i done"
 done
 
 for i in {1..2};
 do
-	python udp_client.py input.txt 2 > res/reorder_20_udp/reorder_20_udp_exp2_"${i}".txt
+	python udp_client.py input.txt 2 > res/reorder_35_udp/reorder_35_udp_exp2_"${i}".txt
 	echo "udp exp2 $i done"
 done
 
 for i in {1..2};
 do
-	python sctp_client.py input.txt 1 > res/reorder_20_sctp/reorder_20_sctp_exp1_"${i}".txt
+	python sctp_client.py input.txt 1 > res/reorder_35_sctp/reorder_35_sctp_exp1_"${i}".txt
 	echo "sctp exp1 $i done"
 done
 for i in {1..2};
 do
-	python sctp_client.py input.txt 2 > res/reorder_20_sctp/reorder_20_sctp_exp2_"${i}".txt
+	python sctp_client.py input.txt 2 > res/reorder_35_sctp/reorder_35_sctp_exp2_"${i}".txt
 	echo "sctp exp2 $i done"
 done
